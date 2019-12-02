583083f0639f48d7c1fd383f85c13733624d2a13
benjewman <bennyadave@gmail.com>
2019-12-02 11:24:40 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code here
+
+function mapToNegativize(sourceArray) {
+    let newArray = []
+    for (i = 0; i < sourceArray.length; i++) {
+        newArray.push(sourceArray[i] * -1)
+    }
+    return newArray
+}
