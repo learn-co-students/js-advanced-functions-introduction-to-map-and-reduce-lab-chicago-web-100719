9f86beb11d885d7a30b1061105ae4581dbacf378
bosmanpa <bsmn.ptrck@gmail.com>
2019-12-02 10:42:40 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
 // Your code here
+function mapToNegativize(array){
+    let x = []
+    for (let i = 0; i < array.length; i++){
+        x.push(-1 * array[i])
+    }
+    return x
+}
+
+function mapToNoChange(array){
+    let x = []
+    for (let i=0; i<array.length; i++){
+        x.push(array[i])
+    }
+    return x
+}
+
+function mapToDouble(array){
+    let x = []
+    for (let i = 0; i < array.length; i++){
+        x.push(2 * array[i])
+    }
+    return x
+}
+
+function mapToSquare(array){
+    let x = []
+    for (let i = 0; i < array.length; i++){
+        x.push(array[i] * array[i])
+    }
+    return x
+}
+
+function reduceToTotal(array, start = 0){
+    let total = start
+    for (let i=0; i<array.length; i++){
+        total = total + array[i]
+    }
+    return total
+}
+
+function reduceToAllTrue(array){
+    for (let i=0; i<array.length; i++){
+        if (!array[i])
+        return false
+    }
+    return true
+}
+
+function reduceToAnyTrue(array){
+    for (let i=0; i<array.length; i++){
+        if (array[i])
+        return true
+    }
+    return false
+}
